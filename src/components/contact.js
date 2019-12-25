dd63cd1ba01cc0c562f091fafe15bb2f74df369a
EllieChen-Git <elliechen.etc@gmail.com>
2019-12-25 18:59:47 +1100

    contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 
 class Contact extends Component {
   render() {
-    return <h1>Contact page</h1>;
+    return (
+      <div className="contact-body">
+        <Grid className="contact-grid">
+          <Cell col={6}>
+            <h2>Ellie Chen</h2>
+            <img src="myAvatar.png" alt="avatar" className="avatar-img" />
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+          </Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+              <List>
+                <ListItem>
+                  <ListItemContent>
+                    <i className="fa fa-phone-square" aria-hidden="true" />
+                    0475-255-677
+                  </ListItemContent>
+                </ListItem>
+                <ListItem>
+                  <ListItemContent>
+                    <i className="fa fa-envelope" aria-hidden="true" />
+                    ellie.tt.chen@gmail.com
+                  </ListItemContent>
+                </ListItem>
+              </List>
+            </div>
+          </Cell>
+        </Grid>
+      </div>
+    );
   }
 }
 
